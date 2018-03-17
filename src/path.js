ab5b94155f61070de1b4f3152765e9386687c528
xrxtzz <xrxtzz@gmail.com>
2018-03-17 00:45:36 -0700

    completed settings for paths

--- src/path.js
+++ src/path.js
@@ -33,6 +33,7 @@
 			_this.data.push({
 				matrix_id: in_matrix,
 				force_id: in_force,
+				center: {x: function() {return matrix.x+num*matrix.unitsize+matrix.unitsize/2;}, y: function() {return matrix.y+num*matrix.unitsize+matrix.unitsize/2;}},
 				pos0: {x: function() {return matrix.x+num*matrix.unitsize+matrix.unitsize/2;}, y: function() {return matrix.y;}},
 				pos1: {x: function() {return matrix.x;}, y: function() {return matrix.y+num*matrix.unitsize+matrix.unitsize/2;}},
 				pos2: {x: function() {return matrix.x+matrix.num_nodes*matrix.unitsize;}, y: function() {return matrix.y+num*matrix.unitsize+matrix.unitsize/2;}},
@@ -41,13 +42,30 @@
 			})
 		});
 	}
-
+	
 	Paths.prototype.render = function() {
 		var _this = this;
+		var generate = function(d) {
+			var result = [];
+			result.push([d.pos_end.x, d.pos_end.y]);
+			var xx = d.pos2.x;
+			var yy = d.pos3.y;
+			//decide the point to use
+			
+			if (d.pos_end.x < _this.x && d.pos_end.y < yy) result.push([d.pos1.x, d.pos1.y]);//left
+			else if (d.pos_end.x > xx && d.pos_end.y > _this.y) result.push([d.pos2.x, d.pos2.y]); //right
+			else if (d.pos_end.x > _this.x && d.pos_end.y < _this.y) result.push([d.pos0.x, d.pos0.y]); //up
+			else if (d.pos_end.x < xx && d.pos_end.y > yy) result.push([d.pos3.x, d.pos3.y]); //down
+			return result;
+		}
+	
+		var line = d3.svg.line(); 
 		this.locallayer.selectAll('path')
 					.data(_this.data)
 					.enter()
-					.append('');
+					.append('path')
+					.attr('d', line(generate(d))
+					.attr('stroke-width', 2);
 		
 	}
 }());
